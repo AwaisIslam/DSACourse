678c30cda2d3de62487bed0786a5ac8ac8db95ba
Jeeny <awais.khan@jeeny.me>
2023-09-21 18:54:10 +0500

    1/Rectangular Star Pattern

--- src/patterns/StarPattern.java
+++ src/patterns/StarPattern.java
@@ -1,4 +1,30 @@
 package patterns;
 
+/**
+ * Print the following pattern
+ * *****
+ * *****
+ * *****
+ * *****
+ */
 public class StarPattern {
+
+    /**
+     * @param n is an integer between 1 and 20
+     */
+    public void printRectangularStar(int n) {
+
+        for (int i = 0; i < n; i++) {
+            // This is the inner loop which here, loops for the columns
+            // as we have to print a rectangular pattern.
+            for (int j = 0; j < n; j++) {
+                System.out.print("* ");
+            }
+
+            // As soon as N stars are printed, we move to the
+            // next row and give a line break otherwise all stars
+            // would get printed in 1 line.
+            System.out.println();
+        }
+    }
 }
