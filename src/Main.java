d4f5d527b9cc7480e15a5291d3d164185e9822ca
Jeeny <awais.khan@jeeny.me>
2023-09-26 00:14:48 +0500

    3/Right Angle Number Pyramid

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
-import patterns.RightAngleTrianglePattern;
+import patterns.RightAngledNumberPyramid;
 
 public class Main {
     public static void main(String[] args) {
         /*StarPattern firstPattern = new StarPattern();
         firstPattern.printRectangularStar(5);*/
 
-        RightAngleTrianglePattern secondPattern = new RightAngleTrianglePattern();
-        secondPattern.printRightAngledRectangularPattern(5);
+        /*RightAngleTrianglePattern secondPattern = new RightAngleTrianglePattern();
+        secondPattern.printRightAngledRectangularPattern(5);*/
+
+        RightAngledNumberPyramid thirdPattern = new RightAngledNumberPyramid();
+        thirdPattern.rightAngleNumberPyramid(5);
     }
 }
--- src/patterns/RightAngledNumberPyramid.java
+++ src/patterns/RightAngledNumberPyramid.java
@@ -1,4 +1,21 @@
 package patterns;
 
+/**
+ * Print the following pattern
+ * 1
+ * 12
+ * 123
+ * 1234
+ * 12345
+ */
 public class RightAngledNumberPyramid {
+
+    public void rightAngleNumberPyramid(int n) {
+        for (int i = 1; i <=n; i++) {
+            for (int j = 1; j <=i; j++) {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
 }
